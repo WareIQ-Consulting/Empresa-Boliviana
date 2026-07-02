c16a28ebd7efe673e23ff9d2dd4a3489dc2dcc90
Smartloop <wilmerfcaussin@gmail.com>
2026-07-01 21:41:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -12,29 +12,21 @@ window.addEventListener("scroll", function(){
 //  MODAL LOGIN CONTROL
 // ==============================
 
-// Botón que abre el modal
 const openModalBtn = document.querySelector("#openModal");
-
-// El modal
-const modal = document.querySelector(".modal");
-
-// Botón cerrar (X)
+const modal = document.querySelector("#loginModal");
 const closeBtn = document.querySelector(".close");
 
-
-// Abrir modal
+// abrir modal
 openModalBtn.addEventListener("click", () => {
     modal.style.display = "flex";
 });
 
-
-// Cerrar modal con la X
+// cerrar con X
 closeBtn.addEventListener("click", () => {
     modal.style.display = "none";
 });
 
-
-// Cerrar modal haciendo clic fuera del contenido
+// cerrar haciendo click fuera
 window.addEventListener("click", (e) => {
     if (e.target === modal) {
         modal.style.display = "none";
