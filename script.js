ba8dc6b3c9e0aa7ba32ab9b2b759c17dd681c251
Smartloop <wilmerfcaussin@gmail.com>
2026-07-06 15:15:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,42 @@ window.addEventListener("scroll", function(){
 
 });
 // ==============================
+// USUARIOS DEL SISTEMA
+// ==============================
+
+const usuarios = [
+
+    {
+        empresa: "Compañía de Alimentos LTDA. (Bolivia)",
+        usuario: "admin_alimentos",
+        password: "123456",
+        dashboard: "dashboard-alimentos.html"
+    },
+
+    {
+        empresa: "Ransa (Bolivia)",
+        usuario: "admin_ransa",
+        password: "123456",
+        dashboard: "dashboard-ransa.html"
+    },
+
+    {
+        empresa: "Sister Moon Hotel (Panamá)",
+        usuario: "admin_sister",
+        password: "123456",
+        dashboard: "dashboard-sister.html"
+    },
+
+    {
+        empresa: "Move (Bolivia)",
+        usuario: "admin_move",
+        password: "123456",
+        dashboard: "dashboard-move.html"
+    }
+
+];
+
+// ==============================
 //  MODAL LOGIN CONTROL
 // ==============================
 
@@ -17,6 +53,11 @@ const closeBtn = document.querySelector(".close");
 const buttons = document.querySelectorAll(".openModal");
 const empresaSeleccionada = document.querySelector("#empresaSeleccionada");
 const empresaSelect = document.querySelector("#empresa");
+
+const loginForm = document.querySelector("#loginForm");
+const usuarioInput = document.querySelector("#usuario");
+const passwordInput = document.querySelector("#password");
+const mensajeLogin = document.querySelector("#mensajeLogin");
 
 // abrir modal desde cualquier botón
 buttons.forEach(btn => {
@@ -54,4 +95,34 @@ document.addEventListener("keydown", (e) => {
         modal.style.display = "none";
     }
 });
+// ==============================
+// LOGIN
+// ==============================
 
+loginForm.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    const empresa = empresaSelect.value;
+    const usuario = usuarioInput.value.trim();
+    const password = passwordInput.value.trim();
+   
+    if(usuario === ""){
+
+    mensajeLogin.style.color = "#ff4d4d";
+    mensajeLogin.textContent = "Ingrese su usuario.";
+
+    return;
+
+}
+
+if(password === ""){
+
+    mensajeLogin.style.color = "#ff4d4d";
+    mensajeLogin.textContent = "Ingrese su contraseña.";
+
+    return;
+
+}
+
+});
